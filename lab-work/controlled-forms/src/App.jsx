7a2a5cad9b286b066d85239bf1870cbd078707be
AnnWL <weiling.ann@gmail.com>
2025-04-16 22:06:05 +0800

    init commit

---
+++ lab-work/controlled-forms/src/App.jsx
@@ -0,0 +1,13 @@
+import Bookshelf from "./components/Bookshelf.jsx";
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
+};
+
+export default App;
